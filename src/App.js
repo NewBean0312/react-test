52581b0d4d2e682bed476ccab271598ca411285f
NewBean0312 <newbean0312@gmail.com>
2023-07-10 19:36:46 +0900

    React Reducer 및 useMemo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import EffectEx from "./EffectEx";
 import ContextEx from "./ContextEx";
 import ReducerEx from "./ReducerEx";
+import MemoEx from "./MemoEx";
 
 const App = () => {
   return (
     <>
       {/* <EffectEx />
-      <ContextEx /> */}
-      <ReducerEx />
+      <ContextEx />
+      <ReducerEx /> */}
+      <MemoEx />
     </>
   );
 };
