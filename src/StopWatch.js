6f61c9ce272a09d56d70038e1553a7d1da11bc33
NewBean0312 <newbean0312@gmail.com>
2023-07-28 17:45:42 +0900

    숫자 재개

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 
 function StopWatch() {
   const [num, setNum] = useState(0);
-
   const timeoutId = setTimeout(() => setNum(num + 1), 1000);
-
   const pause = () => clearTimeout(timeoutId);
+
+  const resume = () => setTimeout(() => setNum(num + 1), 1000);
 
   return (
     <>
       숫자: {num}
       <hr />
       <button onClick={pause()}>일시정지</button>
+      <hr />
+      <button onClick={resume()}>재개</button>
     </>
   );
 }
