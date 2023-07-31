a2c69ff496d3a7e787c56d333eb1bf5539fcf49c
NewBean0312 <newbean0312@gmail.com>
2023-07-31 12:37:37 +0900

    or 연산자

--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -4,20 +4,17 @@ const NumberCounter = () => {
   const [no, setNo] = useState(0);
 
   // 별도의 변수로 분리
-  const noIsEvenDiv =
-    no % 2 == 0 ? (
-      <>
-        <hr />
-        <span>짝수입니다.</span>
-      </>
-    ) : (
-      <></>
-    );
+  const noIsEvenDiv = (
+    <>
+      <hr /> {no % 2 == 0 ? <div>짝수입니다.</div> : <div>홀수입니다.</div>}
+    </>
+  );
 
-  const noIs8MultipleDiv = no % 8 == 0 && (
+  // || : or 연산자
+  const noIs8MultipleDiv = no % 8 == 0 || (
     <>
       <hr />
-      <span>8의 배수입니다.</span>
+      <div>8의 배수가 아닙니다.</div>
     </>
   );
 
