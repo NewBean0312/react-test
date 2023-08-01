901b72af1b32a974df0627e5ac1a910b4b7586f5
NewBean0312 <newbean0312@gmail.com>
2023-08-01 20:21:35 +0900

    상품이미지

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 // import StopWatch from "./StopWatch";
 // import NumberCounter from "./NumberCounter";
-import Popup from "./Popup";
+// import Popup from "./Popup";
 
 const App = () => {
   return (
     <>
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
-      <Popup />
+      {/* <Popup /> */}
+      <div
+        style={{ display: "inline-flex", flexDirection: "column", gap: "10px" }}
+      >
+        <img src="https://picsum.photos/id/201/400/400" />
+        <div style={{ textAlign: "center" }}>MAC OS</div>
+        <div style={{ textAlign: "center" }}>4,340,000</div>
+      </div>
     </>
   );
 };
