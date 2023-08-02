23a5da21280cea98aea1147e90f6beb521ac7130
NewBean0312 <newbean0312@gmail.com>
2023-08-02 20:09:43 +0900

    테일윈드의 container를 사용해서, 내용이 너무 화면 넓게 퍼지는 것을 막음

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
       {/* <Popup /> */}
-      <ProdList />
+      <ProdList className="container mx-auto" />
     </>
   );
 };
