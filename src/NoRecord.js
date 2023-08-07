9e1dbf0565a2641276b008ad895ec0b8a253c8b9
NewBean0312 <newbean0312@gmail.com>
2023-08-07 15:58:16 +0900

    숫자가 교체되는게 아니라, 쌓이도록 설정

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,23 +1,16 @@
 import React, { useState } from "react";
 
 function NoRecord() {
-  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
-
-  setTimeout(() => {
-    setArr([...arr, arr.length + 1]);
-  }, 1000);
-
   const [no, setNo] = useState(0);
-  const [recordNo, setRecordNo] = useState(0);
+  const [recordNos, setRecordNos] = useState([]);
 
   const saveNo = () => {
-    setRecordNo(no);
+    setRecordNos([...recordNos, no]);
   };
 
   return (
     <>
-      {arr.join(",")}
-      {/* <h1>숫자 기록</h1>
+      <h1>숫자 기록</h1>
       <input
         type="number"
         value={no}
@@ -28,7 +21,7 @@ function NoRecord() {
         기록
       </button>
       <hr />
-      기록된 숫자 : {recordNo} */}
+      기록된 숫자 : [{recordNos.join(",")}]
     </>
   );
 }
