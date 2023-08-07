e033ab41ce5155d28586fac8dfc088b7bff148ba
NewBean0312 <newbean0312@gmail.com>
2023-08-07 16:02:00 +0900

    엘리먼트 배열

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ import NoRecord from "./NoRecord";
 import "./App.css";
 
 const App = () => {
+  const arr = [<li>내용1</li>, <li>내용2</li>, <li>내용3</li>];
+
   return (
     <>
+      <ul>{arr}</ul>
+      <hr />
+      <ul>
+        <li>내용1</li>
+        <li>내용2</li>
+        <li>내용3</li>
+      </ul>
+
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
       {/* <Popup /> */}
       {/* <ProdList className="container mx-auto" /> */}
-      <NoRecord />
+      {/* <NoRecord /> */}
     </>
   );
 };
