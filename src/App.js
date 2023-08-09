32e1014c5dab1704f2c4025a9a5d6c6e2da50f1f
NewBean0312 <newbean0312@gmail.com>
2023-08-09 20:59:33 +0900

    데이티UI의 Alert 컴포넌트를 사용하여, 알림UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@
 // import RefEx from "./RefEx";
 //import RefRemoveEx from "./RefRemoveEx";
 import RefModifyEx from "./RefModifyEx";
+import Notice from "./Notice";
 
 
 import "./App.css";
@@ -22,7 +23,8 @@ const App = () => {
       {/* <FormEx /> */}
       {/* <RefEx /> */}
       {/* <RefRemoveEx /> */}
-      <RefModifyEx />
+      {/* <RefModifyEx /> */}
+      <Notice />
     </>
   );
 };
