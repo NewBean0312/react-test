eedf6c6d72ed750b0232f2db638740a648c628e7
NewBean0312 <newbean0312@gmail.com>
2023-08-09 18:02:14 +0900

    숫자 리스트에서, 특정 숫자 제거, 첫 숫자 제거, 마지막 숫자, 제거 기능 추가

--- src/RefEx.js
+++ src/RefEx.js
@@ -3,22 +3,65 @@ import React, { useState, useRef } from "react";
 let RefCallCount = 0;
 
 function RefEx() {
-  RefCallCount++;
-  console.log(`RefEx가 ${RefCallCount}번 실행됨!`);
+  const noInputRef = useRef(null);
+  const [no, setNo] = useState("");
 
-  const [no1, setNo1] = useState(0);
-  const [no2, setNo2] = useState(0);
-  const no3Ref = useRef(0);
+  const [recordedNos, setRecordedNos] = useState([5, 10, 15, 5, 20, 25, 5, 30]);
+
+  const saveNo = () => {
+    if (no === "") {
+      alert("숫자를 입력해주세요.");
+      return;
+    }
+
+    setRecordedNos([...recordedNos, no]);
+    setNo("");
+    noInputRef.current.focus();
+  };
+
+  const removeNo5 = () => {
+    const newRecordedNos = recordedNos.filter((el) => el != 5);
+    setRecordedNos(newRecordedNos);
+  };
+
+  const removeFirst = () => {
+    const newRecordedNos = recordedNos.filter((_, index) => index != 0);
+    setRecordedNos(newRecordedNos);
+  };
+
+  const removeLast = () => {
+    const newRecordedNos = recordedNos.filter(
+      (_, index) => index != recordedNos.length - 1
+    );
+    setRecordedNos(newRecordedNos);
+  };
 
   return (
     <>
-      <button onClick={() => setNo1(no1 + 1)}>숫자 1 증가 : {no1}</button>
-      <br />
-      <button onClick={() => setNo2(no2 + 2)}>숫자 2 증가 : {no2}</button>
-      <br />
-      <button onClick={() => no3Ref.current++}>
-        숫자 3 증가 : {no3Ref.current}
-      </button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveNo();
+        }}
+      >
+        <input
+          type="number"
+          ref={noInputRef}
+          value={no}
+          onChange={(e) => setNo(e.target.valueAsNumber)}
+        />
+        <button type="submit">기록</button>
+      </form>
+      <h1>기록된 숫자</h1>
+      <ul>
+        {recordedNos.map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
+      <hr />
+      <button onClick={removeNo5}>숫자 5 삭제</button>
+      <button onClick={removeFirst}>처음 숫자 삭제</button>
+      <button onClick={removeLast}>마지막 숫자 삭제</button>
     </>
   );
 }
