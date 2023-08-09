f0e8e0de7f265e68ec025f90b7a74bc873cef0e9
NewBean0312 <newbean0312@gmail.com>
2023-08-09 21:32:31 +0900

    Alert 컴포넌트 옵션 작업

--- src/Notice.js
+++ src/Notice.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 
-function Alert() {
+function Alert({color: color_, children}) {
+  const color = color_ ?? "red";
+
   return (
     <>
       <div className="alert alert-info shadow-lg">
-        <div className="text-white">
+        <div className={`text-[${color}]`}>
           <span>
             <i className="fa-solid fa-circle-info"></i>
           </span>
-          <span> New software update available.</span>
+          <span> {children}</span>
         </div>
       </div>
     </>
@@ -18,9 +20,9 @@ function Alert() {
 function Notice() {
   return (
     <>
-      <Alert />
-      <hr />
-      <Alert />
+      <Alert>"안녕" 반가워</Alert>
+      <br />
+      <Alert color="pink">지금은 수업중이야</Alert>
     </>
   );
 }
