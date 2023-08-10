db7245c91977bb59b866161edd8fca98d36bbf4a
NewBean0312 <newbean0312@gmail.com>
2023-08-10 11:42:20 +0900

    NotifyOnce 컴포넌트로 Alert 컴포넌트를 감싸서 숨김

--- src/Notice.js
+++ src/Notice.js
@@ -1,28 +1,51 @@
-import React, { useState } from "react";
+import classnames from "https://cdn.skypack.dev/classnames";
 
-function Alert({color: color_, children}) {
+function NotifyOnce({ children }) {
+  setTimeout(function () {
+    console.log("실행됨");
+  }, 1000);
+
+  return (
+    <div
+      className={classnames(
+        "fixed transition-all right-[10px]",
+        { "top-[-60px]": true },
+        { "top-[10px]": false }
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Alert({ color: color_, children }) {
   const color = color_ ?? "red";
 
   return (
-    <>
-      <div className="alert alert-info shadow-lg">
-        <div className={`text-[${color}]`}>
-          <span>
-            <i className="fa-solid fa-circle-info"></i>
-          </span>
-          <span> {children}</span>
-        </div>
+    <div className="alert alert-info shadow-lg">
+      <div className={`text-[${color}]`}>
+        <span>
+          <i className="fa-solid fa-circle-info"></i>
+        </span>
+        <span> {children}</span>
       </div>
-    </>
+    </div>
   );
 }
 
 function Notice() {
   return (
     <>
-      <Alert>"안녕" 반가워</Alert>
+      <NotifyOnce>
+        <Alert>"안녕" 반가워</Alert>
+      </NotifyOnce>
       <br />
-      <Alert color="pink">지금은 수업중이야</Alert>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        harum nobis nisi qui esse, perferendis cumque ipsum animi corrupti
+        mollitia doloribus dicta sequi quo nihil delectus. Nihil cum eius
+        accusamus?
+      </div>
     </>
   );
 }
