44abe24acac969aed2d36463443d56c20f9f6b29
NewBean0312 <newbean0312@gmail.com>
2023-08-10 20:56:23 +0900

    useMemo를 하기 위한 준비, 1부터 입력값까지의 소수 개수를 알려주는 프로그램 구현에 useEffect를 적용

--- src/App.js
+++ src/App.js
@@ -39,8 +39,13 @@ function App() {
   console.log(`AppCallCount : ${AppCallCount}`);
 
   const [inputedNo, setInputedNo] = useState(0);
+  const [no, setNo] = useState(0);
+  const [primeNumbersCount, setPrimeNumbersCount] = useState();
 
-  const primeNumbersCount = getPrimeNumbersCount(inputedNo);
+  useEffect(() => {
+    const primeNumbersCount = getPrimeNumbersCount(inputedNo);
+    setPrimeNumbersCount(primeNumbersCount);
+  }, [inputedNo]);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -62,6 +67,13 @@ function App() {
 
   return (
     <>
+      <button
+        onClick={() => {
+          setNo(no + 1);
+        }}
+      >
+        번호 : {no}
+      </button>
       <form onSubmit={onSubmit}>
         <input type="number" name="number" placeholder="숫자를 입력해주세요." />
         <input type="submit" value="확인" />
