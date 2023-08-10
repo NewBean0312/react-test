2b9aa8df23703d8eb1c45c6c7ee094ab71696d35
NewBean0312 <newbean0312@gmail.com>
2023-08-10 15:36:19 +0900

    타이밍이 늦어져야 하는 경우, useRef, useEffect의 조합

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import "./App.css";
 
 let AppCallCount = 0;
 
 function App() {
-  useEffect(() => {
-    AppCallCount++;
-    console.log(`App이 ${AppCallCount}번 실행됨!`);
-  }, []);
-
+  const inputNameRef = useRef(null);
+  const inputAgeRef = useRef(null);
   const [no, setNo] = useState(0);
+
+  useEffect(() => {
+    inputNameRef.current.focus();
+  }, []);
 
   return (
     <>
-      <button onClick={() => setNo(no + 1)}>증가 : {no}</button>
+      <input ref={inputNameRef}type="text" placeholder="이름" />
+      <input ref={inputAgeRef} type="number" placeholder="나이" />
+      <button
+        onClick={() => {
+          setNo(no + 1);
+          inputAgeRef.current.focus();
+        }}
+      >
+        증가 : {no}
+      </button>
     </>
   );
 }
