ff6aa68094231c857178caa32c556dbc57b85285
NewBean0312 <newbean0312@gmail.com>
2023-08-10 15:22:49 +0900

    useEffect를 사용하여, 함수의 특정 부분 재실행 방지

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-// import StopWatch from "./StopWatch";
-// import NumberCounter from "./NumberCounter";
-// import Popup from "./Popup";
-// import ProdList from "./ProdList";
-// import NoRecord from "./NoRecord";
-// import FormEx from "./FormEx";
-// import RefEx from "./RefEx";
-//import RefRemoveEx from "./RefRemoveEx";
-import RefModifyEx from "./RefModifyEx";
-import Notice from "./Notice";
-
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
-const App = () => {
+let AppCallCount = 0;
+
+function App() {
+  useEffect(() => {
+    AppCallCount++;
+    console.log(`App이 ${AppCallCount}번 실행됨!`);
+  }, []);
+
+  const [no, setNo] = useState(0);
+
   return (
     <>
-      {/* <StopWatch /> */}
-      {/* <NumberCounter /> */}
-      {/* <Popup /> */}
-      {/* <ProdList className="container mx-auto" /> */}
-      {/* <NoRecord /> */}
-      {/* <FormEx /> */}
-      {/* <RefEx /> */}
-      {/* <RefRemoveEx /> */}
-      {/* <RefModifyEx /> */}
-      <Notice />
+      <button onClick={() => setNo(no + 1)}>증가 : {no}</button>
     </>
   );
-};
+}
 
 export default App;
