738852b7bacbd98d8b9b3f6df8ad240c3e0ade78
NewBean0312 <newbean0312@gmail.com>
2023-08-14 17:22:04 +0900

    라디오 버튼 다루기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Order from "./Order";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [selectedGender, setSelectedGender] = useState("W");
+
   return (
     <>
-      <Order />
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          onChange={(e) => setSelectedGender("M")}
+          checked={selectedGender == "M"}
+        />
+        남성
+        <input
+          type="radio"
+          name="gender"
+          onChange={(e) => setSelectedGender("W")}
+          checked={selectedGender == "W"}
+        />
+        여성
+      </label>
+      <hr />
+      <div>현재 값 : {selectedGender}</div>
     </>
   );
 }
