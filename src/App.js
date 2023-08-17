94d10eaa4f72fdeff66a1d84e916fa4537c1194f
NewBean0312 <newbean0312@gmail.com>
2023-08-17 10:36:20 +0900

    할 일 수정 함수 만들기

--- src/App.js
+++ src/App.js
@@ -13,7 +13,14 @@ function App() {
   const removeTodo = (index) => {
     const newTodos = todos.filter((_, _index) => _index != index);
     setTodos(newTodos);
-  }
+  };
+
+  const modifyTodo = (index, newTodo) => {
+    const newTodos = todos.map((todo, _index) =>
+      _index != index ? todo : newTodo
+    );
+    setTodos(newTodos);
+  };
 
   const onAddBenClick = () => {
     addTodo(todos.length + 1);
@@ -23,11 +30,16 @@ function App() {
     removeTodo(1);
   };
 
+  const onEditBtnClick = () => {
+    modifyTodo(1, "안녕");
+  };
+
   return (
     <>
       <div>{JSON.stringify(todos)}</div>
       <button onClick={onAddBenClick}>추가</button>
       <button onClick={onRemoveBtnClick}>삭제</button>
+      <button onClick={onEditBtnClick}>수정</button>
     </>
   );
 }
