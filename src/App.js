0e043e1ee39cb6d3645ae0f559555e61ebcb2aa6
NewBean0312 <newbean0312@gmail.com>
2023-08-17 10:32:40 +0900

    할 일 삭제 함수 만들기

--- src/App.js
+++ src/App.js
@@ -10,16 +10,24 @@ function App() {
     setTodos(newTodos);
   };
 
-  const onClick = () => {
-    // const newTodos = [...todos, todos.length + 1];
-    // setTodos(newTodos);
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  }
 
+  const onAddBenClick = () => {
     addTodo(todos.length + 1);
+  };
+
+  const onRemoveBtnClick = () => {
+    removeTodo(1);
   };
 
   return (
     <>
-      <button onClick={onClick}>{JSON.stringify(todos)}</button>
+      <div>{JSON.stringify(todos)}</div>
+      <button onClick={onAddBenClick}>추가</button>
+      <button onClick={onRemoveBtnClick}>삭제</button>
     </>
   );
 }
