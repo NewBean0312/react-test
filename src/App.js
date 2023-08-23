ae8ad3ec3dba43e3f46562f118db7947591c7596
NewBean0312 <newbean0312@gmail.com>
2023-08-23 22:52:58 +0900

    각각의 컴포넌트로 분리

--- src/App.js
+++ src/App.js
@@ -58,6 +58,120 @@ const muiThemePaletteKeys = [
   "warning",
 ];
 
+function NewTodoForm({ todosState }) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    form.content.value = form.content.value.trim();
+
+    if (form.content.value.length == 0) {
+      alert("할일을 입력해주세요.");
+      form.content.focus();
+
+      return;
+    }
+
+    todosState.addTodo(form.content.value);
+    form.content.value = "";
+    form.content.focus();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit} className="flex flex-col mt-4 px-4 gap-2">
+        <TextField
+          minRows={3}
+          maxRows={10}
+          multiline
+          autoComplete="off"
+          name="content"
+          label="할일을 입력해주세요."
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained">
+          추가
+        </Button>
+      </form>
+    </>
+  );
+}
+
+function TodoListItem({ todo, index }) {
+  return (
+    <>
+      <li key={todo.id} className="mt-10">
+        <div className="flex gap-2">
+          <Chip
+            label={`번호 : ${todo.id}`}
+            variant="outlined"
+            className="!pt-1"
+          />
+          <Chip
+            label={todo.regDate}
+            variant="outlined"
+            color="primary"
+            className="!pt-1"
+          />
+        </div>
+        <div className="flex shadow mt-4 rounded-[20px]">
+          <Button className="w-[130px] flex-shrink-0 !items-start !rounded-[20px_0_0_20px]">
+            <span
+              className={classNames(
+                "text-3xl",
+                "flex items-center",
+                "h-[50px]",
+                {
+                  "text-[color:var(--mui-color-primary-main)]": index % 2 == 0,
+                },
+                {
+                  "text-[color:#b0b0b0]": index % 2 != 0,
+                }
+              )}
+            >
+              <i class="fa-solid fa-check"></i>
+            </span>
+          </Button>
+          <div className="flex-shrink-0 w-[2px] bg-[#b0b0b0] my-5 mr-6"></div>
+          <div
+            className="whitespace-pre-wrap leading-relaxed
+                  hover:text-[color:var(--mui-color-primary-main)] 
+                  flex-grow my-5 flex items-center"
+          >
+            {todo.content}
+          </div>
+          <Button
+            className="w-[130px] flex-shrink-0 !items-start !rounded-[0__20px_20px_0]"
+            color="inherit"
+          >
+            <span className="text-xl text-[#b0b0b0] flex items-center h-[50px]">
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </span>
+          </Button>
+        </div>
+      </li>
+    </>
+  );
+}
+
+function TodoList({ todosState }) {
+  return (
+    <>
+      <Drawer anchor={"bottom"} open={false} onClose={() => {}}>
+        <div className="p-10">옵션 드로어</div>
+      </Drawer>
+      <div className="mt-4 px-4">
+        <ul>
+          {todosState.todos.map((todo, index) => (
+            <TodoListItem key={todo.id} todo={todo} index={index} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
 function App({ theme }) {
   const todosState = useTodosState();
 
@@ -82,110 +196,17 @@ function App({ theme }) {
     });
   }, []);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    const form = e.target;
-
-    form.content.value = form.content.value.trim();
-
-    if (form.content.value.length == 0) {
-      alert("할일을 입력해주세요.");
-      form.content.focus();
-
-      return;
-    }
-
-    todosState.addTodo(form.content.value);
-    form.content.value = "";
-    form.content.focus();
-  };
-
   return (
     <>
-      <AppBar position="fixed">
+      <AppBar position="static">
         <Toolbar>
           <div className="flex-1"></div>
           <div className="font-bold">HAPPY NOTE</div>
           <div className="flex-1"></div>
         </Toolbar>
       </AppBar>
-      <Toolbar />
-
-      <form onSubmit={onSubmit} className="flex flex-col mt-4 px-4 gap-2">
-        <TextField
-          minRows={3}
-          maxRows={10}
-          multiline
-          autoComplete="off"
-          name="content"
-          label="할일을 입력해주세요."
-          variant="outlined"
-        />
-        <Button type="submit" variant="contained">
-          추가
-        </Button>
-      </form>
-      <Drawer anchor={"bottom"} open={true} onClose={() => {}}>
-        <div className="p-10">옵션 드로어</div>
-      </Drawer>
-      <div className="mt-4 px-4">
-        <ul>
-          {todosState.todos.map((todo, index) => (
-            <li key={todo.id} className="mt-10">
-              <div className="flex gap-2">
-                <Chip
-                  label={`번호 : ${todo.id}`}
-                  variant="outlined"
-                  className="!pt-1"
-                />
-                <Chip
-                  label={todo.regDate}
-                  variant="outlined"
-                  color="primary"
-                  className="!pt-1"
-                />
-              </div>
-              <div className="flex shadow mt-4 rounded-[20px]">
-                <Button className="w-[130px] flex-shrink-0 !items-start !rounded-[20px_0_0_20px]">
-                  <span
-                    className={classNames(
-                      "text-3xl",
-                      "flex items-center",
-                      "h-[50px]",
-                      {
-                        "text-[color:var(--mui-color-primary-main)]":
-                          index % 2 == 0,
-                      },
-                      {
-                        "text-[color:#b0b0b0]": index % 2 != 0,
-                      }
-                    )}
-                  >
-                    <i class="fa-solid fa-check"></i>
-                  </span>
-                </Button>
-                <div className="flex-shrink-0 w-[2px] bg-[#b0b0b0] my-5 mr-6"></div>
-                <div
-                  className="whitespace-pre-wrap leading-relaxed
-                  hover:text-[color:var(--mui-color-primary-main)] 
-                  flex-grow my-5 flex items-center"
-                >
-                  {todo.content}
-                </div>
-                <Button
-                  className="w-[130px] flex-shrink-0 !items-start !rounded-[0__20px_20px_0]"
-                  color="inherit"
-                >
-                  <span className="text-xl text-[#b0b0b0] flex items-center h-[50px]">
-                    <i class="fa-solid fa-ellipsis-vertical"></i>
-                  </span>
-                </Button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <NewTodoForm todosState={todosState} />
+      <TodoList todosState={todosState} />
     </>
   );
 }
