0c579b2ff6b5d5f7d046b9e5a6b1f6d622caaaba
NewBean0312 <newbean0312@gmail.com>
2023-09-05 15:02:20 +0900

    Page 이동 기록 History

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -4,7 +4,7 @@ import { Button } from "@mui/material";
 function HomePage() {
   return (
     <>
-      <h1>홈 페이지</h1>
+      <h1>Home Page</h1>
     </>
   );
 }
@@ -12,7 +12,7 @@ function HomePage() {
 function AboutPage() {
   return (
     <>
-      <h1>About 페이지</h1>
+      <h1>About Page</h1>
     </>
   );
 }
@@ -20,41 +20,58 @@ function AboutPage() {
 function LoginPage() {
   return (
     <>
-      <h1>Login 페이지</h1>
+      <h1>Login Page</h1>
     </>
   );
 }
 
-export default function RouterEx() {
-  const [url, setUrl] = useState("home");
+function useHistory() {
+  const initialUrl = "home";
+  const [url, setUrl] = useState(initialUrl);
+  const [historyUrls, setHistoryUrls] = useState([initialUrl]);
 
+  const movePage = (url) => {
+    setUrl(url);
+    setHistoryUrls([url, ...historyUrls]);
+  };
+
+  return {
+    url,
+    movePage,
+    historyUrls,
+  };
+}
+
+export default function RouterEx() {
+  const history = useHistory();
   return (
     <>
-      <div className="p-5">현재 주소 : {url}</div>
+      <div className="p-5">현재 주소 : {history.url}</div>
+      <div className="p-5">History : {history.historyUrls.join(",")}</div>
       <ul className="flex gap-3 p-5">
         <li
-          onClick={() => setUrl("home")}
+          onClick={() => history.movePage("home")}
           className="hover:text-red-300 cursor-pointer"
         >
           <Button variant="contained">Home</Button>
         </li>
         <li
-          onClick={() => setUrl("about")}
+          onClick={() => history.movePage("about")}
           className="hover:text-red-300 cursor-pointer"
         >
           <Button variant="contained">About</Button>
         </li>
         <li
-          onClick={() => setUrl("login")}
+          onClick={() => history.movePage("login")}
           className="hover:text-red-300 cursor-pointer"
         >
           <Button variant="contained">Login</Button>
         </li>
       </ul>
       <div className="p-5">
-        {url == "home" && <HomePage />}
-        {url == "about" && <AboutPage />}
-        {url == "login" && <LoginPage />}
+        {history.url == "home" && <HomePage />}
+        {history.url == "about" && <AboutPage />}
+        {history.url == "login" && <LoginPage />}
       </div>
     </>
   );
